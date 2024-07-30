2f7542d562b6ce4c62a7db895d276a8560927d03
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-07-30 16:25:04 -0300

    Dividir a informacoes em blocos que podemos manipular
    Ainda precisamos fazer um pequeno processamento mas, em termos gerais,
    estamos quase prontos.

--- pkgs/main.py
+++ pkgs/main.py
@@ -7,15 +7,18 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from unidecode import unidecode
 
-wschar = re.compile(r"\xa0")
+spc_dash_spc_word = re.compile(r"^((i{1,3}|i?v) - ([0-9]+)?)([a-z ]+)")
+spc_dash = re.compile(r"^(i{1,3}|i?v) -$")
+spc_dash_spc = re.compile(r"^(i{1,3}|i?v) - ([0-9]+)$")
 
-doc_url = (
+doc_url = (  # {{{
     "https://docs.google.com/document/d/e/"
     "2PACX-1vTh8sOz5qjNOK_zXUFtnAnn3pgNmBv"
     "wQMCFWvxKh9TxjBvgqmllpakoITbAeBPHe6Lv"
     "kqnek5-MceZ1/pub?landscape=true"
-)
+)  # }}}
 
 
 def main():
@@ -24,21 +27,29 @@ def main():
     options.add_argument("--headless=new")
 
     driver = webdriver.Chrome(options=options)
-
     driver.get(doc_url)
-
     driver.implicitly_wait(0.5)
-
     beautiful_soup = BeautifulSoup(driver.page_source, "lxml")
-
     driver.implicitly_wait(0.5)
-
     driver.quit()
 
-    for table in beautiful_soup.find_all("table"):
-        for row in table.find_all("tr"):
-            data = [wschar.sub(r"", col.text) for col in row.find_all("td")]
-            print(data)
+    with open("brew/sicadplus.py", "w") as pyfile:
+        for table in beautiful_soup.find_all("table"):
+            for row in table.find_all("tr"):
+                info = [
+                    unidecode(col.text.lower()).strip()
+                    for col in row.find_all("td")
+                ]
+                if len(info) == 1:
+                    info = [
+                        spc_dash_spc_word.search(info[0]).group(1).strip(),
+                        spc_dash_spc_word.search(info[0]).group(4).strip(),
+                    ]
+                    if spc_dash.search(info[0]):
+                        info[0] = spc_dash.search(info[0]).group(1)
+                    elif spc_dash_spc.search(info[0]):
+                        info[0] = spc_dash_spc.sub(r"\1-\2", info[0])
+                print(info, file=pyfile)
 
 
 if __name__ == "__main__":
