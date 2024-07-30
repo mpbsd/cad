889347e07821ca119ccdf47788e974b2b6cdc847
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-07-30 12:05:59 -0300

    add selenium script that gathers source of info

---
+++ pkgs/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+doc_url = (
+    "https://docs.google.com/document/d/e/"
+    "2PACX-1vTh8sOz5qjNOK_zXUFtnAnn3pgNmBv"
+    "wQMCFWvxKh9TxjBvgqmllpakoITbAeBPHe6Lv"
+    "kqnek5-MceZ1/pub?landscape=true"
+)
+
+
+def main():
+    options = Options()
+    options.add_argument("--disable-extensions")
+    options.add_argument("--headless=new")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(doc_url)
+
+    driver.implicitly_wait(0.5)
+
+    beautiful_soup = BeautifulSoup(driver.page_source, "lxml")
+
+    driver.implicitly_wait(0.5)
+
+    driver.quit()
+
+    for table in beautiful_soup.find_all("table"):
+        for row in table.find_all("tr"):
+            data = [col.text for col in row.find_all("td")]
+            print(data)
+
+
+if __name__ == "__main__":
+    main()
