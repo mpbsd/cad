e9b2906ab229bfb35635d31c68d7cff88fc45f69
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-07-30 18:23:02 -0300

    Leave a CSV for now

--- pkgs/main.py
+++ pkgs/main.py
@@ -9,16 +9,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from unidecode import unidecode
 
-spc_dash_spc_word = re.compile(r"^((i{1,3}|i?v) - ([0-9]+)?)([a-z ]+)")
+spc_dash_spc_word = re.compile(r"^((i{1,3}|i?v) - ([0-9]+)?)([a-z -]+)")
 spc_dash = re.compile(r"^(i{1,3}|i?v) -$")
 spc_dash_spc = re.compile(r"^(i{1,3}|i?v) - ([0-9]+)$")
 
-doc_url = (  # {{{
+doc_url = (
     "https://docs.google.com/document/d/e/"
     "2PACX-1vTh8sOz5qjNOK_zXUFtnAnn3pgNmBv"
     "wQMCFWvxKh9TxjBvgqmllpakoITbAeBPHe6Lv"
     "kqnek5-MceZ1/pub?landscape=true"
-)  # }}}
+)
 
 
 def main():
@@ -33,7 +33,7 @@ def main():
     driver.implicitly_wait(0.5)
     driver.quit()
 
-    with open("brew/sicadplus.py", "w") as pyfile:
+    with open("brew/A2RC182017.csv", "w") as csvfile:
         for table in beautiful_soup.find_all("table"):
             for row in table.find_all("tr"):
                 info = [
@@ -49,7 +49,10 @@ def main():
                         info[0] = spc_dash.search(info[0]).group(1)
                     elif spc_dash_spc.search(info[0]):
                         info[0] = spc_dash_spc.sub(r"\1-\2", info[0])
-                print(info, file=pyfile)
+                    pr = f"{info[0]};{info[1]};;;;"
+                else:
+                    pr = f";{info[0]};{info[1]};{info[2]};{info[3]};{info[4]}"
+                print(pr, file=csvfile)
 
 
 if __name__ == "__main__":
